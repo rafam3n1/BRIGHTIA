8e52660a3cf0f49f83e55f2804d26943d7ce875c
rafam3n1 <tornevirtual@gmail.com>
2023-02-19 12:47:28 -0300

    prompt map

--- server/server.js
+++ server/server.js
@@ -15,6 +15,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const promptMap = {
+  'O que é Bright?': 'Bright Cloud games é um serviço streaming de jogos com servidores no Brasil',
+  // adicione mais perguntas e respostas aqui
+}
+
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from CodeX!'
@@ -25,26 +30,25 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    let response;
-    if (prompt.toLowerCase() === "o que é bright?") {
-      response = "Bright Cloud games é um serviço streaming de jogos com servidores no Brasil";
+    if (promptMap[prompt]) {
+      res.status(200).send({
+        bot: promptMap[prompt]
+      });
     } else {
-      response = await openai.createCompletion({
+      const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: `${prompt}`,
-        temperature: 0.7, // Higher values means the model will take more risks.
-        max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-        top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-        frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-        presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+        temperature: 0.7,
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
       });
-      response = response.data.choices[0].text;
+
+      res.status(200).send({
+        bot: response.data.choices[0].text
+      });
     }
-
-    res.status(200).send({
-      bot: response
-    });
-
   } catch (error) {
     console.error(error)
     res.status(500).send(error || 'Something went wrong');
