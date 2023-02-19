ab8d1afe8ee764b53461874ef802efbf6c7319c5
rafam3n1 <tornevirtual@gmail.com>
2023-02-19 13:08:57 -0300

    novo teste promptmap

--- server/server.js
+++ server/server.js
@@ -15,41 +15,42 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const promptMap = new Map([
-  ["o que é bright?", "Bright Cloud games é um serviço streaming de jogos com servidores no Brasil."],
-]);
+const promptMap = {
+  'o que é bright?': 'Bright Cloud games é um serviço streaming de jogos com servidores no Brasil'
+};
 
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from CodeX!'
-  })
-})
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const prompt = req.body.prompt.toLowerCase();
 
-    if (promptMap.has(prompt)) {
-      res.status(200).send({
-        bot: promptMap.get(prompt),
-      });
+    let response;
+    if (promptMap[prompt]) {
+      response = promptMap[prompt];
     } else {
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
+      response = await openai.createCompletion({
+        model: "text-davinci-002",
         prompt: `${prompt}`,
         temperature: 0.7,
-        maxTokens: 60,
-        n: 1,
-        stop: "\n",
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
       });
-
-      res.status(200).send({
-        bot: response.data.choices[0].text.trim(),
-      });
+      response = response.data.choices[0].text;
     }
 
+    res.status(200).send({
+      bot: response
+    });
+
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).send(error || 'Something went wrong');
   }
 });
